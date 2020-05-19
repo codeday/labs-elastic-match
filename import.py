6c3685fffe4dd07318bff02e319e7b4e422c79fc
Jake <royalcows9@gmail.com>
2020-05-19 13:44:47 -0700

    Add InnerDoc for projects

--- import.py
+++ import.py
@@ -1,1 +1,47 @@
 # Ensure that skip this and come back is false, for there is a lot of empty fields
+
+from elasticsearch_dsl import connections
+from models import Mentor, Project
+
+conn = connections.create_connection(hosts=['10.0.3.33:9200'], timeout=20)
+
+mentor = Mentor(
+    email = "johnpeter@srnd.org",
+    phone = "(111) 111-1111",
+    timezone = -8,  # America pacific is UTC-8
+    two_projects = True,
+    is_recruited_mentor = True,
+    why_mentor = "I like to mentor junior people and share what I've learned with them",
+    past_mentoring = "Been a TA in college",
+    linkedin = "https://www.example.com",
+    company = "SRND",
+    where_work_text = "Seattle WA"
+    where_work_point =
+    role =
+    industry =
+    mentor_experience =
+    grow_up_text =
+    grow_up_number =
+
+    # Next section will define weights if possible
+    student_specific_tool_experience =
+    student_specific_tool_desire =
+    student_similar_upbringing =
+    student_similar_background =
+    student_alma_mater =
+    student_desire_company =
+    student_underrepresented =
+    student_continue_check_in_ok =
+    student_timezone_outside_workday =
+
+    # More personal information on mentor
+    pronoun =
+    ethnicity =
+    can_commit =
+    company_hour_matching =
+
+    # These two should be yes
+    signature_captured = True,
+    entry_reviewed = True
+)
+
--- models.py
+++ models.py
@@ -1,17 +1,22 @@
-from elasticsearch_dsl import Document, Text, GeoPoint, Date, Integer, Keyword, Boolean, connections, Short
+from elasticsearch_dsl import Document, Text, GeoPoint, Date, Integer, Keyword, Boolean, connections, Short, Index, \
+    InnerDoc, Nested
+
+
+class Project(InnerDoc):
+    project_proposal = Text(required=True)  # This should be a searchable corpus of text, so Text it is
+    project_tags = Keyword(multi=True,
+                           required=True)  # This should be a list, so be sure to assign values to it as a list
+    num_students_confirmed = Short()  # Should be 0-3
+    # https://github.com/elastic/elasticsearch-dsl-py/issues/95
 
 
 class Mentor(Document):
     email = Keyword(required=True)  # Keyword should be used since this is not a body of text
-    phone = Keyword(required=True)  # Keyword should be used since this is not a body of text, seems better than number types
+    phone = Keyword(
+        required=True)  # Keyword should be used since this is not a body of text, seems better than number types
     timezone = Short()  # This will have a number relative to UTC
-    project_1_proposal = Text(required=True)  # This should be a searchable corpus of text, so Text it is
-    project_1_tags = Keyword(multi=True, required=True)  # This should be a list, so be sure to assign values to it as a list
-    # https://github.com/elastic/elasticsearch-dsl-py/issues/95
-    project_2_proposal = (Text())  # This should be a searchable corpus of text, so Text it is
-    project_2_tags = Keyword(multi=True)  # This should be a list, so be sure to assign values to it as a list
-    one_or_both = Boolean(required=True)
-    is_recruited_mentor = Boolean(required=True)
+    two_projects = Boolean(required=True)  # True if this person agreed to run two groups
+    is_recruited_mentor = Boolean(required=True)  # IDK what this means
     why_mentor = Text(required=True)  # Again, is freeform and non-structured
     past_mentoring = Keyword(multi=True)  # This should be a list, so be sure to assign values to it as a list
     linkedin = Keyword(required=True)
@@ -23,7 +28,7 @@ class Mentor(Document):
     mentor_experience = Integer()
     grow_up_text = Keyword()
     grow_up_number = Short()  # Census num for region the person grew up in. 1-3 is urban, 4-6 is suburbs, 7-9 is rural
-    
+
     # Next section will define weights if possible
     student_specific_tool_experience = Short()  # How important already knowing tools is
     student_specific_tool_desire = Short()  # How important willingness to learn specific tools is 
@@ -45,19 +50,22 @@ class Mentor(Document):
     signature_captured = Boolean()
     entry_reviewed = Boolean()
 
+    # Nested project document, up to two should exist
+    projects = Nested(Project)
 
-def setup():
-    """
-    Create the index in elastic, not really sure how this template bit works and how it differs from my above setup,
-    however it was in the most similar example.
-    """
+    def add_project(self, proposal: str, tags: list):
+        self.projects.append(
+            Project(project_proposal=proposal, project_tags=tags)
+        )
 
-    # create an index template, not really sure what this does?
-    index_template = Mentor._index.as_template("base")
-    # upload the template into elasticsearch
-    # potentially overriding the one already there
-    index_template.save()
 
-    # Create the index
-    if not Mentor._index.exists():
-        Mentor.init()
+if __name__ == '__main__':
+    from elasticsearch_dsl import connections
+
+    connections.create_connection(hosts=['10.0.3.33:9200'], timeout=20)
+
+    mentors = Index("mentors")
+
+    mentors.document(Mentor)
+
+    mentors.create()
