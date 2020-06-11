f5fa272cb94016c1b399c74a077bf5cc67a816ba
Jake <royalcows9@gmail.com>
2020-06-11 15:38:25 -0700

    Refactor the testing and setup stuff, updated csv import to the right schema

--- helpers.py
+++ helpers.py
@@ -1,6 +1,12 @@
+"""Assorted helper funcs for various data manipulation things.
+
+Many may now be unused since a lot of the data manipulation was offloaded to before I see it,
+but I'm keeping these around for now
+"""
 import pytz, datetime
 from pyzipcode import ZipCodeDatabase
 from typing import Optional
+import re
 
 
 def zip_to_timeone(zipcode) -> int:  # zipcode: zipcode
@@ -10,6 +16,7 @@ def zip_to_timeone(zipcode) -> int:  # zipcode: zipcode
 
 
 def parse_tz(tz_str: str) -> Optional[int]:
+    """Parses the string forms of time zones from the data into ints"""
     if tz_str == 'America - Eastern':
         return -5
     if tz_str == 'America - Pacific':
@@ -26,25 +33,120 @@ def parse_tz(tz_str: str) -> Optional[int]:
         return None
 
 
-def location_text_to_geopoint(place: str):  # -> GeoPoint https://tinyurl.com/pkqn4be | None
-    pass
+def fix_education_dict(dictionary) -> dict:
+    """Fixes the education date ranges to be graduation year"""
+    for k, v in dictionary.items():
+        # If a year is givin in the format yyyy-yyyy, need to parse to work with elastic
+        if re.match(r"[0-9]{4}-[0-9]{4}", v):
+            dictionary[k] = v.split("-")[1]
+    return dictionary
 
 
-def point_to_urbanity(point) -> int:  # takes in geopoint, returns census urbanity measurement
+def parse_number(text):
+    """Parses and cleans up some messy number fields"""
+    _pattern = r"""(?x)       # enable verbose mode (which ignores whitespace and comments)
+        ^                     # start of the input
+        [^\d+-\.]*            # prefixed junk
+        (?P<number>           # capturing group for the whole number
+            (?P<sign>[+-])?       # sign group (optional)
+            (?P<integer_part>         # capturing group for the integer part
+                \d{1,3}               # leading digits in an int with a thousands separator
+                (?P<sep>              # capturing group for the thousands separator
+                    [ ,.]                 # the allowed separator characters
+                )
+                \d{3}                 # exactly three digits after the separator
+                (?:                   # non-capturing group
+                    (?P=sep)              # the same separator again (a backreference)
+                    \d{3}                 # exactly three more digits
+                )*                    # repeated 0 or more times
+            |                     # or
+                \d+                   # simple integer (just digits with no separator)
+            )?                    # integer part is optional, to allow numbers like ".5"
+            (?P<decimal_part>     # capturing group for the decimal part of the number
+                (?P<point>            # capturing group for the decimal point
+                    (?(sep)               # conditional pattern, only tested if sep matched
+                        (?!                   # a negative lookahead
+                            (?P=sep)              # backreference to the separator
+                        )
+                    )
+                    [.,]                  # the accepted decimal point characters
+                )
+                \d+                   # one or more digits after the decimal point
+            )?                    # the whole decimal part is optional
+        )
+        [^\d]*                # suffixed junk
+        $                     # end of the input
     """
-    NOT WORKING, LOWER PRIORITY
+    match = re.match(_pattern, text)
+    if match is None or not (match.group("integer_part") or
+                             match.group("decimal_part")):  # failed to match
+        return None  # consider raising an exception instead
 
-    Census num for region the person grew up in.
+    num_str = match.group("number")  # get all of the number, without the junk
+    sep = match.group("sep")
+    if sep:
+        num_str = num_str.replace(sep, "")  # remove thousands separators
 
-    1-3 is urban, 4-6 is suburbs, 7-9 is rural. -1 on error, 0 outside US
+    if match.group("decimal_part"):
+        point = match.group("point")
+        if point != ".":
+            num_str = num_str.replace(point, ".")  # regularize the decimal point
+        return float(num_str)
 
-    :param point: A geopoint
-    :return:
+    return int(num_str)
+
+
+def fix_main_dict(dictionary) -> dict:
     """
-    pass
+    Fixes issues with strings of floats that cannot be directly cast to an int, and with bad booleans
+
+    """
+    for k, v in dictionary.items():
+        if v == "Yes":
+            dictionary[k] = True
+        if v == "No":
+            dictionary[k] = False
+        if v == "1.0" or v == "2.0" or v == "3.0" or v == "0.00":
+            dictionary[k] = int(float(v))
+    return dictionary
+
+
+def validate_entry(dictionary: dict) -> str:
+    """
+    Checks to see if an entry has all the items it needs to have to be valid.
+
+    This includes:
+    * At least one full project
+    * Skip this false
+    * Signature true
+    * Code of Conduct Agreement
+
+    Some of this is required information, but I'm already writing the check to deal with lack of projects so :shrug:
+
+    :param dictionary: The input dictionary, must be specifically the mentor dictionary
+    :return: str of numbers. 0 if OK, otherwise the string will contain the following numbers if there is such an error:
+     1 if no projects, 2 if skip this is true, 3 if no signature, 4 if code of conduct (i.e. "14" would mean no
+     projects and no code of conduct.
+    """
+    error_str = ""
+
+    if dictionary['FirstProjectProposal'] == "" or dictionary['TagThisProject'] == 'TagThisProject':
+        error_str += "1"
+    if dictionary['SkipThisAndComeBackToIt'] is True:
+        error_str += "2"
+    if dictionary['Signature'] != "Captured":
+        error_str += "3"
+    if dictionary['IAgreeToTheCodeOfConduct'] is not True:
+        error_str += "4"
+    if error_str == "":
+        error_str = "0"
+    return error_str
 
 
 def listify(tags_str: str) -> list:
+    """Takes a string list and makes it into a list.
+        Now that I'm writing this docstring, this probably could have been a typecast...
+    """
     return [i.strip() for i in tags_str.split(",")]
 
 
---
+++ testing and setup/import_from_csv.py
@@ -0,0 +1,47 @@
+from elasticsearch_dsl import connections
+from models import MentorProject
+from helpers import *
+from csv import DictReader
+
+conn = connections.create_connection(hosts=['10.0.3.33:9200'], timeout=20)
+
+with open("../older_schema/data/CodeLabs_Mentor_Application (1).xlsx - CodeLabsMentorApplication.csv", mode="r") as mentor_csv_file:
+    mentor_dict = DictReader(mentor_csv_file)
+
+    num_added_successfully = 0
+    total_loops = 0
+
+    for mentor_data in mentor_dict:
+        total_loops += 1
+        mentor_data = fix_main_dict(mentor_data)
+
+        error_str = validate_entry(mentor_data)
+        if error_str != "0":
+            # TODO: Figure out how to really log this so we can come back, probably some logging file or something
+            print("AHHHHHHHHHHHHH! This person's data is incomplete! I'm going to pass on adding this one, "
+                  "but here's all the info I have on it. Please follow up! Also, the error code is: " + error_str)
+            print(mentor_data)
+            continue
+
+        project_elastic = MentorProject(
+            id=mentor_data["id"],
+            name=mentor_data["name"],
+            company=mentor_data["company"],
+            bio=mentor_data["bio"],
+            backgroundRural=mentor_data["backgroundRural"],
+            preferStudentUnderRep=mentor_data["preferStudentUnderRep"],  # (0-2)
+            preferToolExistingKnowledge=mentor_data["preferToolExistingKnowledge"],
+            okExtended=mentor_data["okExtended"],
+            timezone=mentor_data["timezone"],
+            proj_id=mentor_data["proj_id"],
+            proj_description=mentor_data["proj_description"],
+            proj_tags=mentor_data["proj_tags"],
+            studentsSelected=0,
+            okTimezoneDifference=mentor_data["okTimezoneDifference"],
+            isBeginner=mentor_data["isBeginner"],
+        )
+
+        project_elastic.save()
+
+        num_added_successfully += 1
+        # print(f"Another one down! So far, {num_added_successfully} out of {total_loops} have succeeded.")
