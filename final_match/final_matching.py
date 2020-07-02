355b849a2f717176600df28cd054b536626d457d
Jake <royalcows9@gmail.com>
2020-07-01 19:15:06 -0700

    Moved matching to a new file, it mostly works pretty well

---
+++ final_match/final_matching.py
@@ -0,0 +1,103 @@
+"""Build the combined data dictionary and the student placement dictionary"""
+import json
+from final_match.helpers import *
+from copy import deepcopy
+
+project_docs = open("./mentor_index_dump_2", "r", encoding="utf-8")
+project_size_data = json.load(open("./mentors_size_data.json", "r", encoding="utf-8"))
+project_size_dict = {project["proj_id"]: project["proj_size_remaining"] for project in project_size_data}
+
+all_project_data = {}
+student_placements = {}
+for line in project_docs:
+    project_doc = json.loads(line)
+    if project_doc["id"] == 'recRJGXhTr9lEq1tO':
+        continue
+    id_ = project_doc["id"]
+    proj_size = project_size_dict[id_]
+    if proj_size == 0:
+        continue
+    all_project_data[id_] = project_doc
+    all_project_data[id_]["proj_size_remaining"] = proj_size
+    all_project_data[id_]["num_first_choice"] = len(
+        [choice for choice in project_doc.get('listStudentsSelected') if choice["choice"] == 1])
+
+    student_placements[id_] = {"students": [], "proj_capacity": project_size_dict[id_]}
+
+starting_students = []
+for i in [[student["student_id"] for student in project["listStudentsSelected"]] for k, project in
+          all_project_data.items()]:
+    for j in i:
+        starting_students.append(j)
+num_starting_students = len(set(starting_students))
+
+# Do step 1
+for id, project in all_project_data.items():
+    if project["proj_size_remaining"] == project["num_first_choice"]:
+        all_project_data, student_placements = place_students_of_choice(all_project_data, student_placements, id, 1,
+                                                                        project["proj_size_remaining"])
+print("Step 1")
+print(check_for_lost_students(num_starting_students, all_project_data, student_placements))
+print("------------")
+
+# Do step 2
+for id, project in all_project_data.items():
+    if project["proj_size_remaining"] >= project["num_first_choice"]:
+        # print("Project is smaller than first choice")
+        all_project_data, student_placements = place_students_of_choice(all_project_data, student_placements, id, 1,
+                                                                        project["proj_size_remaining"])
+print("Step 2")
+print(check_for_lost_students(num_starting_students, all_project_data, student_placements))
+print("------------")
+
+# Do step 3
+_all_project_data = deepcopy(all_project_data)
+for id, project in _all_project_data.items():
+    if project["proj_size_remaining"] >= project["num_first_choice"]:
+        all_project_data, student_placements = place_students_of_choice_balanced(all_project_data, student_placements,
+                                                                                 id, [2, 15],
+                                                                                 project["proj_size_remaining"])
+print("Step 3")
+print(check_for_lost_students(num_starting_students, all_project_data, student_placements))
+print("------------")
+
+# # Do step 4
+_all_project_data = deepcopy(all_project_data)
+for id, project in _all_project_data.items():
+    all_project_data, student_placements = place_students_of_choice_balanced(all_project_data, student_placements, id,
+                                                                             [1, 2], project["proj_size_remaining"])
+print("Step 4")
+print(check_for_lost_students(num_starting_students, all_project_data, student_placements))
+print("------------")
+
+for i in range(3, 16, 4):
+    _all_project_data = deepcopy(all_project_data)
+    for id, project in _all_project_data.items():
+        if project["proj_size_remaining"] >= project["num_first_choice"]:
+            # print(f"Project is smaller than {i} choice")
+            all_project_data, student_placements = place_students_of_choice_balanced(all_project_data,
+                                                                                     student_placements, id,
+                                                                                     [i, i + 1, i + 2, i + 3],
+                                                                                     project["proj_size_remaining"])
+print("Step 4.5")
+print(check_for_lost_students(num_starting_students, all_project_data, student_placements))
+print("------------")
+
+# Step 5 - Go through all missed projects?
+
+
+# Check for issues
+count = 0
+for id, project in all_project_data.items():
+    if project["proj_size_remaining"] >= len(project["listStudentsSelected"]):
+        count += project["proj_size_remaining"]
+print("Unfilled Spots:" + str(count))
+
+count = 0
+for id, project in all_project_data.items():
+    if 0 < len(project["listStudentsSelected"]):
+        count += 1
+        # count += len(project["listStudentsSelected"])
+print("Students left over:" + str(count))
+
+print("lol")
---
+++ final_match/helpers.py
@@ -0,0 +1,131 @@
+from typing import Tuple
+from copy import deepcopy
+
+
+def check_for_lost_students(starting_students: int, project_data_dict: dict, placement_dict: dict) -> int:
+    students = []
+    for i in [[student for student in project["students"]] for k, project in placement_dict.items()]:
+        for j in i:
+            students.append(j)
+    for i in [[student["student_id"] for student in project["listStudentsSelected"]] for k, project in
+              project_data_dict.items()]:
+        for j in i:
+            students.append(j)
+    return starting_students - len(set(students))
+
+
+def unwrap_student_data(project_data_dict: dict) -> dict:
+    unwrapped_dict = {}
+    intermediate_list = []
+    list_students = set([])
+    for proj_id, project in project_data_dict.items():
+        for student in project['listStudentsSelected']:
+            intermediate_list.append({student["student_id"]: {proj_id: student["choice"]}})
+            list_students.add(student["student_id"])
+    for student_id in list_students:
+        student_proj_list = []
+        for proj_data in intermediate_list:
+            student_proj_list.append()
+        unwrapped_dict[student_id] = [proj_data for proj_data in intermediate_list if proj_data.keys()[0] == ]
+
+
+
+
+def measure_match_effectiveness(placement_dict: dict, unwrapped_student_dict: dict) -> int:
+
+
+
+
+def count_student_votes(project_data_dict: dict, student_id: str) -> int:
+    student_votes = 0
+    for k, project in project_data_dict.items():
+        student_votes += len(
+            [student for student in project['listStudentsSelected'] if student["student_id"] == student_id])
+    return student_votes
+
+
+def remove_student(project_data_dict: dict, student_id: str) -> dict:
+    """Removes the provided student ID from the all_project_data dict"""
+    for id, project in project_data_dict.items():
+        project_data_dict[id]["listStudentsSelected"] = [
+            student
+            for student in project["listStudentsSelected"]
+            if student["student_id"] != student_id
+        ]
+    return project_data_dict
+
+
+def place_student(
+        project_data_dict: dict, placement_dict: dict, project_id: str, student_id: str
+) -> Tuple[dict, dict]:
+    """Places the student in the project, handles removing the student from project data as well as decrementing
+    needed students and placing needed info in placement_dict. Also removes completed projects from the project data and
+    decrements "num_first_choice" if needed.
+
+    :return project_data_dict, placement_dict
+    """
+    choice = 0
+    for student in project_data_dict[project_id]["listStudentsSelected"]:
+        if student["student_id"] == student_id:
+            choice = student["choice"]
+
+    placement_dict[project_id]["students"].append(student_id)
+    project_data_dict = remove_student(project_data_dict, student_id)
+    project_data_dict[project_id]["proj_size_remaining"] -= 1
+    if choice == 1:
+        project_data_dict[project_id]["num_first_choice"] -= 1
+
+    if project_data_dict[project_id]["proj_size_remaining"] <= 0:
+        del project_data_dict[project_id]
+
+    return project_data_dict, placement_dict
+
+
+def place_students_of_choice(
+        project_data_dict: dict,
+        placement_dict: dict,
+        project_id: str,
+        choice: int,
+        num: int,
+) -> Tuple[dict, dict]:
+    """Will place the first `num` students of choice `choice` on a project, or will place until all students of `choice`
+    have been placed. Chooses between valid students by picking those who appear the least frequently in other remaining
+    votes."""
+
+    counter = 0
+    modified_project_data_dict = deepcopy(project_data_dict)
+    for student in project_data_dict[project_id]["listStudentsSelected"]:
+        if counter >= num:
+            break
+        if student["choice"] == choice:
+            place_student(modified_project_data_dict, placement_dict, project_id, student["student_id"])
+            counter += 1
+            # print(f"added to {project_id} for a total of {counter}")
+        if project_data_dict[project_id] is None:
+            break
+    return modified_project_data_dict, placement_dict
+
+
+def place_students_of_choice_balanced(
+        project_data_dict: dict,
+        placement_dict: dict,
+        project_id: str,
+        choice: list,
+        num: int,
+) -> Tuple[dict, dict]:
+    """Will place the first `num` students of choice `choice` on a project, or will place until all students of `choice`
+    have been placed. Chooses between valid students by picking those who appear the least frequently in other remaining
+    votes."""
+
+    counter = 0
+    matching_student_frequency = sorted(
+        {student["student_id"]: count_student_votes(project_data_dict, student["student_id"])
+         for student in project_data_dict[project_id]["listStudentsSelected"]
+         if student["choice"] in choice}.items(), key=lambda x: x[1])
+    for i in range(num):
+        if i >= len(matching_student_frequency):
+            break
+        project_data_dict, placement_dict = place_student(project_data_dict, placement_dict, project_id,
+                                                          matching_student_frequency[i][0])
+
+    return project_data_dict, placement_dict
