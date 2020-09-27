5b434e4a9a7f96a5f888d0463764bdf78cf79cd1
Jake <royalcows9@gmail.com>
2020-09-26 17:39:46 -0700

    Sample data can be matched successfully

--- matching_new/new_matching_scratchbook.py
+++ matching_new/new_matching_scratchbook.py
@@ -21,25 +21,17 @@ mentor_project_capacities = {
 }
 """
 from matching.games import HospitalResident
+from json import load, dumps
+
+with open("../data/testing_student_votes.json") as student_votes_fp:
+    student_votes = load(student_votes_fp)
 
 student_preferences = {
-    "A": ["X1", "X2"],
-    "B": ["Y2", "X1"],
-    "C": ["X1", "Y1"],
-    "D": ["Y2", "Y1"],
+    student: [list(mentor.keys())[0] for mentor in votes]
+    for student, votes in student_votes[0].items()
 }
-mentor_project_preferences = {
-    "X1": ["C", "A", "B"],
-    "X2": ["A"],
-    "Y1": ["C", "D"],
-    "Y2": ["B", "D"]
-}
-mentor_project_capacities = {
-    'X1': 1,
-    'X2': 1,
-    'Y1': 1,
-    'Y2': 1
-}
+mentor_project_preferences = student_votes[1]
+mentor_project_capacities = {mentor: 5 for mentor, student in student_votes[1].items()}
 
 game = HospitalResident.create_from_dictionaries(
     student_preferences, mentor_project_preferences, mentor_project_capacities
@@ -49,3 +41,17 @@ game.solve(optimal="resident")
 print(game.check_validity())
 print(game.check_stability())
 print(game.matching)
+
+print(
+    dumps(
+        dict(
+            zip(
+                [mentor.name for mentor in game.matching.keys()],
+                [
+                    [student.name for student in mentor_match]
+                    for mentor_match in game.matching.values()
+                ],
+            )
+        )
+    )
+)
